a8285a70a80071265f2b8dad5d540520b2befb83
EJOweb <erik@ejoweb.nl>
2016-02-19 18:02:12 +0100

    Removed renamed files...

--- content/kennisbank-archief-content.php
+++
@@ -1,7 +0,0 @@
-<article <?php hybrid_attr( 'post' ); ?>>
-
-		<header class="entry-header">
-			<h4 <?php hybrid_attr( 'entry-title' ); ?>><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark" itemprop="url"><?php the_title(); ?></a></h4>
-		</header><!-- .entry-header -->
-
-</article><!-- .entry -->
