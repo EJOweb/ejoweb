7d6fc55313c6d8759cdf02a2d0434fe0aaa8e391
EJOweb <erik@ejoweb.nl>
2016-04-05 17:24:21 +0200

    Chaotic in-between commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 /* EJOweb gulpfile
- * v20160127
+ * v20160405
  *
- * npm install gulp gulp-util gulp-rename gulp-concat gulp-sass gulp-autoprefixer gulp-uglify gulp-jshint --save-dev
+ * npm install --save-dev gulp gulp-util gulp-plumber gulp-rename gulp-concat gulp-sass gulp-autoprefixer gulp-coffee gulp-uglify gulp-jshint
  * 
  * gulp-postcss overwegen: https://github.com/postcss/gulp-postcss
  */
 
-//* Package variables
+/** 
+ * Package Variables
+ */
+
+/* Gulp */
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+
+/* CSS */
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+
+/* Javascript */
+var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+
+/** 
+ * Config Variables
+ */
 
 //* Config
 var sass_dir = './_build/scss/';
@@ -23,35 +37,47 @@ var js_dir = './_build/js/';
 //* Autoprefixer browser support
 var browser_support = 'last 2 version'; 
 
-//* Create expanded and minified stylesheet at the same time (performance is fast using libsass)
+/** 
+ * Code
+ */
+
+//* Create expanded and minified stylguesheet at the same time (performance is fast using libsass)
 //* In case of error, show it only once
-gulp.task('sass', function () {
+gulp.task('sass:main', function () {
 
     //* Create expanded stylesheet
+    // gulp.src([sass_dir + 'theme.scss'])
+    //     .pipe(sass({
+    //         outputStyle: 'expanded'
+    //     }))
+    //     .pipe(autoprefixer({ remove: false, browsers:[browser_support] }))
+    //     .on('error', gutil.log) // On error: show log and continue
+    //     .pipe(gulp.dest('./assets/css/'));
+
     gulp.src([sass_dir + 'theme.scss'])
+        .pipe(plumber())
         .pipe(sass({
             outputStyle: 'expanded'
         }))
         .pipe(autoprefixer({ remove: false, browsers:[browser_support] }))
-        .on('error', gutil.log) // On error: show log and continue
         .pipe(gulp.dest('./assets/css/'));
 
     //* Create minified stylesheet
-    gulp.src([sass_dir + 'theme.scss'])
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(autoprefixer({ remove: false, browsers:[browser_support] }))
-        .on('error', gutil.noop) // On error: just continue because log is already shown above
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./assets/css/'));
+    // gulp.src([sass_dir + 'theme.scss'])
+    //     .pipe(sass({
+    //         outputStyle: 'compressed'
+    //     }))
+    //     .pipe(autoprefixer({ remove: false, browsers:[browser_support] }))
+    //     .on('error', gutil.noop) // On error: just continue because log is already shown above
+    //     .pipe(rename({
+    //         suffix: '.min'
+    //     }))
+    //     .pipe(gulp.dest('./assets/css/'));
 });
 
 //* Create expanded and minified stylesheet at the same time (performance is fast using libsass)
 //* In case of error, show it only once
-gulp.task('sass-editor', function () {
+gulp.task('sass:editor', function () {
 
     //* Create expanded stylesheet
     gulp.src([sass_dir + 'editor-style.scss'])
@@ -75,15 +101,15 @@ gulp.task('sass-editor', function () {
         .pipe(gulp.dest('./assets/css/'));
 });
 
-// Lint Task
-gulp.task('lint', function() {
+// Javascript
+gulp.task('js:main', function() {
+
+    // Lint
     gulp.src(js_dir + '*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
-});
 
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
+    // Concatenate & Minify JS
     gulp.src(js_dir + '*.js')
         .pipe(concat('theme.js'))
         .pipe(gulp.dest('./assets/js/'))
@@ -95,10 +121,17 @@ gulp.task('scripts', function() {
 });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch( js_dir + '*.js', ['lint', 'scripts'] );
-    gulp.watch( sass_dir + '**/*.scss', ['sass', 'sass-editor'] );
+gulp.task('sass:watch', function() {
+    // gulp.watch( sass_dir + '**/*.scss', ['sass:main', 'sass:editor'] );
+    gulp.watch( sass_dir + '**/*.scss', ['sass:main'] );
+});
+
+// Watch Files For Changes
+gulp.task('js:watch', function() {
+    gulp.watch( js_dir + '*.js', ['js'] );
 });
 
 //* Default task
-gulp.task('default', ['lint', 'sass', 'sass-editor', 'scripts', 'watch']);
+gulp.task('default', ['sass:main', 'sass:editor', 'js:main', 'sass:watch', 'js:watch']);
+gulp.task('sass', ['sass:main', 'sass:watch']);
+gulp.task('js', ['js:main', 'js:watch']);
